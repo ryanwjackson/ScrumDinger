955f5308ed7ea47182e642ee47d44f1fe9366e77
Ryan Jackson <ryanwjackson@gmail.com>
2023-10-09 16:25:12 -0700

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Ryan Jackson on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
