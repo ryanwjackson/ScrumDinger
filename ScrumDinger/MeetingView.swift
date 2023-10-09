48b570bd24ab71d893755fe17c015448a1b89749
Ryan Jackson <ryanwjackson@gmail.com>
2023-10-09 16:53:21 -0700

    Initial commit

---
+++ ScrumDinger/MeetingView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  ScrumDinger
+//
+//  Created by Ryan Jackson on 10/9/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MeetingView()
+}
